40a2e0c6545668be7e7954afeeeda1471b49304f
Cedeno718 <cedeno718@morrisville.edu>
2024-09-28 18:06:26 -0400

    Namespaces – Organizing Code:
    Organized scripts into namespaces.

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -1,14 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
    void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "ground")
-        {   
-        Debug.Log("WIPEOUT!");
+        {
+            Debug.Log("WIPEOUT! >.<");
+            SceneManager.LoadScene(0);
         }
     }
 }
